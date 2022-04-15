7f270698a617ec60dcb2669c3feae6b61d53d7bc
Hamid <hamid.darzi.87@gmail.com>
2022-04-15 17:07:01 +0430

    add sout(I am in branch_a)

--- src/com/hb/test/Main.java
+++ src/com/hb/test/Main.java
@@ -1,7 +1,13 @@
 package com.hb.test;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am in branch_a");
+        List<Integer> integers = List.of(1, 2, 3, 4);
+        for (Integer i : integers) {
+            System.out.println("Integer value: " + i);
+        }
     }
 }
